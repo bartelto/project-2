c16ce04f9880ff784e4cddf30d80a44f5c0f15c7
Miles Standoffish <toddnan@gmail.com>
2019-09-25 17:35:39 -0500

    user matches are populating

--- public/js/matches.js
+++ public/js/matches.js
@@ -1,26 +1,27 @@
 /* eslint-disable indent */
 /* eslint-disable prettier/prettier */
 
-console.log("inside matches.js");
+console.log("loaded matches.js");
 
 // grab the user profile template
-let $matchesSection = $("#matches-display-section");
-let $profileTemplate = $("matches-template");
+let $profileTemplate = $("#matches-template");
 
 updateMatches();
 
-
 function updateMatches() {
-
-
   // use AJAX to send a request for match data
-  $.get("api/matches/1", function(data) {
-    console.log(data);
+  $.get("api/matches/2", function(data) {
 
-    // clone the template and add it to the page
-    $profileTemplate.clone().appendTo("#matches-display-section"); 
-
-    
+    if (data.length === 0) {
+      $("#matches-display-section").append("<h3>Sorry, you have no matches!<h3>");
+    } else {
+      // copy the template's contents and add them to a new <div>
+      data.forEach( function(match) {
+        let newMatch = $("<div>").attr("data-id",match.id).html($profileTemplate.html()).appendTo("#matches-display-section");
+        newMatch.find(".match-name").text(match.screenName); 
+        newMatch.find(".match-image").attr("src",match.imageUrl);
+      });
+    }
   });
 
 
