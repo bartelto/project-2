4f6e222d1f5b0ff0f2ac76bbd4df9c4e6a4b5d5b
Miles Standoffish <toddnan@gmail.com>
2019-09-28 17:48:37 -0500

    populating match modal with user data

--- public/js/matches.js
+++ public/js/matches.js
@@ -6,8 +6,29 @@ console.log("loaded matches.js");
 // grab the user profile template
 let $profileTemplate = $("#matches-template");
 
+
 updateMatches();
 
+$(document).click(".match-display-section", function(event) {
+  $selectedProfile = $(event.target).closest(".match-display-section");
+  $("#modal-name").text($selectedProfile.find(".match-name").text());
+  $("#modal-image").attr("src",$selectedProfile.find(".match-image").attr("src"));
+  $("#modal-games-list").empty();
+  $.get(`api/gameprefs/${$selectedProfile.attr("data-user-id")}`, function(data) {
+    data.forEach( function(game) {
+      let newGame = $("<li>").text(game.gameName);
+      $("#modal-games-list").append(newGame);
+    });
+  });
+
+  $('#match-modal').modal("show");
+
+})
+/*
+$('#match-modal').on('show.bs.modal', function (event) {
+  console.log("modal showing");
+});
+*/
 function updateMatches() {
   // use AJAX to send a request for match data
   $.get("api/matches/1", function(data) {
@@ -19,11 +40,14 @@ function updateMatches() {
       data.forEach( function(match) {
         //console.log(match);
         let newMatch = $profileTemplate.children().clone().removeAttr("hidden").appendTo("#matches-display-section");
+        //let newMatch = $("<div>").attr("data-id",match.id).html($profileTemplate.html()).appendTo("#matches-display-section");
+        newMatch.attr("data-user-id",match.id);
         newMatch.find(".match-name").text(match.screenName); 
         newMatch.find(".match-image").attr("src",match.imageUrl);
       });
     }
   });
+
 
 
 }
