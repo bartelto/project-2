cc113adac180fc016f2f25a6f883417e7796e76f
Miles Standoffish <toddnan@gmail.com>
2019-09-29 09:16:57 -0500

    exploring login data

--- public/js/login.js
+++ public/js/login.js
@@ -121,3 +121,19 @@ $loginBtn.on("click", logIn);
 $logoutBtn.on("click", logOut);
 
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      /*var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;*/
+      console.log(user.email + " is logged in.");
+    } else {
+      // User is signed out.
+      console.log("No user logged in.");
+    }
+  });
